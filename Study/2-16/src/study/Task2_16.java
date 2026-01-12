223b1098ea49f51ce07db988dee0db6a37f42af3
yoyo100455 <6253.yui@gmail.com>
2026-01-12 19:48:55 +0900

    2-16

--- Study/2-16/src/study/Task2_16.java
+++ Study/2-16/src/study/Task2_16.java
@@ -13,8 +13,8 @@ public class Task2_16 {
 
         //① firstNameとlastNameという名前の変数を定義し、
         // firstNameには自分の名前、lastNameには自分の名字で初期化しなさい
-        String firstName = "名前";
-        String lastName ="苗字";
+        String firstName = "小川";
+        String lastName ="唯";
         
         //getNameメソッドの呼び出しと出力
         System.out.println(getName(firstName, lastName));
---
+++ Study/chapter2/src/chapter2/Ex1.java
@@ -0,0 +1,12 @@
+package chapter2;
+
+public class Ex1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello java");
+        
+        // TODO 自動生成されたメソッド・スタブ
+
+    }
+
+}
