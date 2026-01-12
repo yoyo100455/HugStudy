ec26c0c3df9763cbad8161ba62eba5e969c77beb
yoyo100455 <6253.yui@gmail.com>
2026-01-12 19:51:28 +0900

    2-16

--- Study/2-16/src/study/Task2_16.java
+++ Study/2-16/src/study/Task2_16.java
@@ -13,8 +13,8 @@ public class Task2_16 {
 
         //① firstNameとlastNameという名前の変数を定義し、
         // firstNameには自分の名前、lastNameには自分の名字で初期化しなさい
-        String firstName = "小川";
-        String lastName ="唯";
+        String firstName = "唯";
+        String lastName ="小川";
         
         //getNameメソッドの呼び出しと出力
         System.out.println(getName(firstName, lastName));
