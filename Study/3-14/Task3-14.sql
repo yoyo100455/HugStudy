f15aedf67adf62a00eb7e29e8ec5694f7cec6d9e
yoyo100455 <6253.yui@gmail.com>
2026-02-07 09:48:35 +0900

    3-14

--- Study/3-14/Task3-14.sql
+++ Study/3-14/Task3-14.sql
@@ -28,6 +28,8 @@ WHERE store_code = 'EA03';
 INSERT INTO goods_table (goods_code, goods_name, price, update_day)
 VALUES ('M001', 'マフラー', 4500, CURRENT_DATE);
 
+SELECT * FROM goods_table WHERE goods_code = 'M001';
+
 -- 7. 店舗テーブルに下記の3つのデータを同時に追加して下さい。
 INSERT INTO store_table (store_code, store_name, store_nameAbc, update_day)
 VALUES
@@ -35,18 +37,24 @@ VALUES
 ('WE03', '梅田店', 'UMEDETEN', '2013-02-01'),
 ('WE04', '福岡店', 'FUKUOKATEN', '2014-05-01');
 
-
+SELECT * FROM store_table WHERE store_code IN ('EA09', 'WE03', 'WE04');
 
 -- 8. 在庫テーブルの在庫数が20以上の商品の在庫数を50に更新して下さい。※実行後の「データ出力」画面も貼付すること。
 UPDATE stock_table 
 SET quantity = 50 
 WHERE quantity >= 20;
 
+SELECT * FROM stock_table WHERE quantity = 50;
+
 -- 9. 在庫テーブルの商品（'S987'）かつ、店舗（'EA01'）の在庫数を10増やす更新を行って下さい。※実行後のSELECT結果も貼付すること。
 UPDATE stock_table 
 SET quantity = quantity + 10 
 WHERE goods_code = 'S987' AND store_code = 'EA01';
 
+SELECT * FROM stock_table WHERE goods_code = 'S987' AND store_code = 'EA01';
+
 -- 10. 商品コード（'Z939'）かつ、店舗コード（'EA04'）に関連する在庫情報を在庫テーブルから削除して下さい。※実行後の「データ出力」画面も貼付すること。
 DELETE FROM stock_table 
 WHERE goods_code = 'Z939' AND store_code = 'EA04';
+
+SELECT * FROM stock_table WHERE goods_code = 'Z939' AND store_code = 'EA04';
